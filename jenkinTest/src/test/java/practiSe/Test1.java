6e00f94f6fc6952d800d529d831de13389e09e93
sher01234 <shersinghinda01@gmail.com>
2023-05-24 17:26:32 +0530

    jenkin commit

---
+++ jenkinTest/src/test/java/practiSe/Test1.java
@@ -0,0 +1,36 @@
+package practiSe;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+	@Test
+	public void demo() throws Throwable {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		FileInputStream fis =new FileInputStream("./data/ComonFile.properties");
+		Properties p=new Properties();
+		p.load(fis);
+		System.out.println("login to application");
+		String Url = p.getProperty("url");
+		String UserName = p.getProperty("un");
+		String PassWord = p.getProperty("pw");
+		
+		driver.get(Url);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.out.println("hellow");
+		System.out.println("welcome to jenkin");
+		
+	}
+
+}
